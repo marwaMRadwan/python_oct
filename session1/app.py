35c057540fe8aa2c5956c0ec427cac0eccbccf6c
marwaMRadwan <marwaradwan872@hotmail.com>
2025-10-30 15:29:53 +0300

    s1

---
+++ session1/app.py
@@ -0,0 +1,187 @@
+# print("hello")
+# print("hello")
+# print("hello")
+# print("hello")
+
+# x = 5
+# x = True
+# x = "marwa"
+
+# print(x)
+
+# a = b = c = 3
+
+# print(" a = ", a)
+
+# print(type(x))
+# print(type(a))
+
+# Numbers (int, float)
+# strings
+# booleans
+# lists
+# tuple
+# set
+# dictionary
+# a=1J
+# print(type(a))
+
+# a="marwa"
+# b='marwa \
+# radwan'
+# c='''marwa radwan marwa radwan marwa radwan marwa radwan marwa radwan 
+# marwa radwan marwa radwan marwa radwan marwa radwan marwa radwan marwa radwan 
+# marwa radwan marwa radwan marwa radwan marwa radwan marwa radwan '''
+# print(b)
+
+# name = input("please enter your name: ")
+# salary = float(input("enter your salary"))
+
+# print(type(salary))
+
+# print("hello ", name, "and test data ", name)
+
+# template_string = f"hello, {name} and test data {name}"
+# print(template_string)
+# print(f"hello, {name} and test data {name}")
+
+# print(f"user name is {name} and salary is {salary:.2f}")
+
+# x = y = z = 5
+
+# x,y,z = 1,2,3
+# print(y)
+
+# data = ["a", "b", "c", "d"] #collection
+# x,y,z,w = data  # unpacking
+# print(data)
+
+
+# Hello 'marwa' from python
+# print('hello \'marwa\' from python')
+
+# txt = "             Winds whisper softly through trees beneath a glowing moon.             "
+# txt= txt.strip()
+# print(txt[:2])
+# print(txt.upper())
+# print(txt.lower())
+# print(txt.strip())
+# print(txt.split(" "))
+
+#test   => tset
+txt ="marwa m radwan"
+# my_list = list(txt)
+# print(my_list)
+
+# txt = txt.replace("m", "mohammed")
+# print(txt)
+
+# ind = txt.find("m", 1, 5)
+# txt[2] = "mohammed"
+
+# txt.charAt(2) = "test"
+# print(txt)
+
+# txt.insert(2, "hello")
+# print(txt)
+
+# txt = "marwa m radwan"
+# loc = txt.find("m", 1)
+# newtxt = txt[0:loc] + " Mohammed " + txt[loc+1:]
+# print(newtxt)
+
+# Arithmatic operators
+# = + - * - -val ** / // %
+# a =  9
+# b = 5
+# result = a - b
+# result = a % b
+# print(result)
+# x = "A"
+# print(5 * x)
+# c= input("c = ") 
+# f = float(c) * 9 / 5 + 32
+# print(f)
+
+# comparison operators
+#  ==  != > < >= <=  True False
+# x=5
+# y=6
+# print( x = y )
+
+# logical operators
+# and or not
+
+# x = None
+
+# if x :
+#     print("hello1")
+
+# if not x :
+#     print("hello2")
+
+# bitwise ope
+
+
+
+# x = 6
+# if x>10:
+#     print("x greater than 10")
+# elif x>5:
+#     print("greater than  5 and less than  10")
+# else:
+#     print("x less than 10")
+
+
+chinese_zodiac = ["Rat", "Ox","Tiger", "Rabbit", "Dragon", "Snake", "Horse","Goat","Monkey", "Rooster","Dog","Pig"]
+# print(chinese_zodiac[-3])
+
+# year_of_birth = int(input("enter your year "))
+# print(int(year_of_birth)%12)
+# print( chinese_zodiac[ year_of_birth % 12 ] )
+# for ele in chinese_zodiac:
+#     print(ele)
+
+# print(len(chinese_zodiac))
+
+# nums = [1,2,3,4, 3.5]
+# print(sum(nums))
+# mylist =[]
+# for n in range(0,10):
+#     x = float(input("enter a val: "))
+#     mylist.append(x)
+# print(mylist)
+
+# name = "marwa radwan"
+# for a in name:
+#     print(a)
+
+# for i in range(6,1,-1):
+#     if i==3:
+#         continue
+#     print(i)
+
+
+# num = int(input("enter a number: "))
+# for x in range(1,13):
+#     print(f"{x} X {num} = {x*num}")
+
+# num = int(input("enter a number: "))
+# res = 1
+# for x in range(1, num+1 ):
+#     res *=x
+# print(res)
+
+# name = "abc"
+
+# print( name[::-1] )
+
+# print(name)
+
+
+index =1 
+while index < 10 :
+    print(index)
+    index+=2
+else:
+    print("ended")
